130f323ca6f3df7c8146d5e3c35383320079bde7
saleenabajracharya <saleena.bajracharya12@gmail.com>
2020-05-14 10:21:37 +0545

    sql added

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,9 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
+    @if(count($posts) > 0)
+      
+      <div class="container" style="margin-left:10px;margin-top:10px">
+        <div class="row">
+            @foreach($posts as $post)
+                <figure class="col-md-4">
+                    <a class="black-text" href="/posts/{{$post->id}}"
+                    data-size="1600x1067">
+                    <img class="img-fluid" style="height:300px;width:300px" src="/storage/cover_images/{{$post->cover_image}}">
+                    
+                    </a>
+                    <h3 class="text-center my-3">{{$post->title}}</h3>
+                    <div class="interaction">
+                        
+                         {{-- <a href="{{ route('like', ['post_id' => $post->id]) }}" class="post-item">Like</a>| --}}
+                       
+                        <hr>
+                    </div>
+                    {{-- <small>Written on {{$post->created_at}} by {{$post->user->name}}</small> --}}
+            </figure>
+            @endforeach
+        </div>
+      </div>
+          
+          {{$posts->links()}}
+          @else
+              <p>No posts found</p>
+          @endif
+ 
+    
+
+
+@endsection
+{{-- @extends('layouts.app')
+
+@section('content')
     <h1>Posts</h1>
     @if(count($posts) > 0)
-        @foreach($posts as $post)
+        {{-- @foreach($posts as $post)
             <div class="well">
                 <div class="row">
                     <div class="col-md-4 col-sm-4">
@@ -16,9 +52,9 @@
                     </div>
                 </div>
             </div>
-        @endforeach
+        @endforeach --}}
         {{$posts->links()}}
     @else
         <p>No posts found</p>
     @endif
-@endsection
+@endsection --}}
